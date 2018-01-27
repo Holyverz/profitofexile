7b30a1fb81d9bbc68e83cf617378845f2e2d9a4e
Holyverz <oli_rouillard@hotmail.com>
2018-01-27 17:22:37 -0500

    Firebase added

--- server.js
+++ server.js
@@ -3,16 +3,37 @@ const express = require("express");
 const config = require("./config.json");
 const app = express();
 const PORT = config.PORT || 3000; //if doesnt exist port = 3000
+const firebase = require("./firebase.js");
 const POENINJA_BASEURL =
     "http://poe.ninja/api/Data/GetCurrencyOverview?league=";
 app.use(express.static("public"));
-
 app.get("/poeninja", (req, res) => {
     //req.body.league GETLEAGUE
+    const dbReference = firebase.database().ref(); //reference to the database
+
     const jsonNinja = fetch(`${POENINJA_BASEURL}Abyss`)
         .then(data => data.json())
-        .then(json => res.send(json));
+        .then(json => {
+            const filteredJson = json.lines.reduce((acc, cur) => {
+                acc.push({
+                    name: cur.currencyTypeName,
+                    chaos: cur.chaosEquivalent
+                });
+                return acc;
+            }, []);
+            dbReference.child("/poeninja/currency/").set(filteredJson);
+            res.send(filteredJson);
+        });
 });
+// app.get("/fb", (req, res) => {
+//     const dbReference = firebase.database().ref(); //reference to the database
+//     dbReference.child("/cashmoneyz/vaal").set("1 HH");
+//     dbReference
+//         .child("/cashmoneyz/")
+//         .once("value")
+//         .then(s => console.log(s.val()));
+// });
 
 app.listen(PORT);
 console.log(`Listening on, Port : ${PORT}`);
+console.log(firebase.app().name);
